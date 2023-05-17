1faf025c61445bec8c2832281238d88f3d3184c8
2021038122 <2021038122@hanyang.ac.kr>
2023-05-17 11:39:13 +0900

    week7

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, re, next) {
-    resizeBy.send('hello world!!');
+    res.send('hello world!!');
 });
 
 module.exports = router;
